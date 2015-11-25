1692b861298dd88f6e0c6ed4a7399f49eb97cee3
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-25 19:39:06 +0300

    Add babel for tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
-var gulp = require('gulp'),
-  qunit = require('node-qunit-phantomjs');
+const gulp = require('gulp');
+const qunit = require('node-qunit-phantomjs');
+const sourceMaps = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
 
 gulp.task('test', function() {
   qunit('./spec/tests.html', { 'verbose': true });
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['spec/**/*.*', 'src/**/*.*'], ['test']);
+gulp.task("build", function () {
+  return gulp.src("src/**/*.js") //get all js files under the src
+    .pipe(sourceMaps.init()) //initialize source mapping
+    .pipe(babel()) //transpile
+    .pipe(sourceMaps.write(".")) //write source maps
+    .pipe(gulp.dest("build")); //pipe to the destination folder
 });
 
-gulp.task('default', ['watch', 'test']);
+gulp.task("build_spec", function () {
+  return gulp.src("spec/**/*.js") //get all js files under the src
+    .pipe(sourceMaps.init()) //initialize source mapping
+    .pipe(babel()) //transpile
+    .pipe(sourceMaps.write(".")) //write source maps
+    .pipe(gulp.dest("build_spec")); //pipe to the destination folder
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['spec/**/*.*', 'src/**/*.*'], ['build', 'build_spec', 'test']);
+});
+
+gulp.task('default', ['build', 'build_spec', 'test', 'watch']);
