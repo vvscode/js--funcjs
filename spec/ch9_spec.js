434aef0bc978ef7e5963345af51ae21fdef5d92e
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-05 20:55:16 +0300

    Data/flow orientation

--- spec/ch9_spec.js
+++ spec/ch9_spec.js
@@ -8,6 +8,32 @@ module("Chapter 9", {
 });
 
 
-test('LasyChain', function() {
+test('lazyChain', function() {
+  var deferredSorts = _.map([[2, 1, 3], [7, 7, 1], [0, 9, 5]], deferredSort);
+  equal(_.isArray(deferredSorts), true);
+  equal(deferredSorts.length, 3);
+  deferredSorts.forEach(function(item) {
+    equal(typeof item.force, 'function');
+    equal(typeof item.invoke, 'function');
+  });
 
+  var lazyOp = lazyChain([2, 1, 3])
+    .invoke('concat', [7, 7, 8, 9, 0])
+    .invoke('sort');
+
+  deepEqual(lazyOp.force(), [0, 1, 2, 3, 7, 7, 8, 9]);
+
+  var deferredSorts2 = _.map([[2, 1, 3], [7, 7, 1], [0, 9, 5]], deferredSort);
+  deepEqual(_.map(deferredSorts2, force), [[1, 2, 3], [1, 7, 7], [0, 5, 9]]);
+});
+
+test('validateTripleStore', function() {
+  deepEqual(
+    validateTripleStore([[2, 1, 3], [7, 7, 1], [0, 9, 5]]),
+    [[2, 1, 3], [7, 7, 1], [0, 9, 5]]
+  );
+});
+
+test('processTriples', function() {
+  equal(processTriples("[[2,1,3], [7,7,1], [0,9,5]]"), "1,7,9");
 });
---
+++ src/ch9.js
@@ -0,0 +1,84 @@
+function lazyChain(obj) {
+  var calls = [];
+  return {
+    invoke: function(methodName /* args */) {
+      var args = _.rest(arguments);
+      calls.push(function(target) {
+        var meth = target[methodName];
+        return meth.apply(target, args);
+      });
+      return this;
+    },
+    force: function() {
+      return _.reduce(calls, function(ret, thunk) {
+        return thunk(ret);
+      }, obj);
+    }
+  };
+}
+
+function validator(message, fun) {
+  var f = function(/* args */) {
+    return fun.apply(fun, arguments);
+  };
+  f['message'] = message;
+  return f;
+}
+
+function condition1(/* validators */) {
+  var validators = _.toArray(arguments);
+  return function(fun, arg) {
+    var errors = mapcat(function(isValid) {
+      return isValid(arg) ? [] : [isValid.message];
+    }, validators);
+    if (!_.isEmpty(errors))
+      throw new Error(errors.join(", "));
+    return fun(arg);
+  };
+}
+
+function cat() {
+  var head = _.first(arguments);
+  if (existy(head))
+    return head.concat.apply(head, _.rest(arguments)); else
+    return [];
+}
+
+function mapcat(fun, coll) {
+  return cat.apply(null, _.map(coll, fun));
+}
+
+function deferredSort(ary) {
+  return lazyChain(ary).invoke('sort');
+}
+
+function force(thunk) {
+  return thunk.force();
+}
+
+var validateTriples = validator(
+  "Each array should have three elements", function(arrays) {
+    return _.every(arrays, function(a) {
+      return a.length === 3;
+    });
+  });
+var validateTripleStore = partial1(condition1(validateTriples), _.identity);
+
+function postProcess(arrays) {
+  return _.map(arrays, second);
+}
+
+function str(arr) {
+  return arr.join(',');
+}
+
+function processTriples(data) {
+  return pipeline(data
+    , JSON.parse
+    , validateTripleStore
+    , deferredSort
+    , force
+    , postProcess
+    , invoker('sort', Array.prototype.sort)
+    , str);
+}
